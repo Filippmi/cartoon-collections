4dde1729ee990d2c11e3b92110cb961945ca4dd8
Filipp Minayev <Filippmi@gmail.com>
2020-11-12 19:02:40 -0700

    commit tree ca61d01cc64900cb298b32f4f7f3bd6f66c8f86b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end
+
+def summon_captain_planet(planateer_calls)# code an argument here
+  planateer_calls.map{|call| call.capitalize + '!'}
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(calls)# code an argument here
+  calls.any? {|call| call.length > 4 }
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |possible|
+    cheese_types.include?(possible)
+  end
+  # the array below is here to help
+
 end
